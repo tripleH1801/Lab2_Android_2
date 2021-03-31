89259495f7023ae99986f542d9c071737c1a4b46
tripleh1801 <hoanghhh.01257989756@gmail.com>
2021-03-31 09:29:29 +0700

    first commit

---
+++ app/src/main/java/com/example/lab1_18056521_hahuyhoang/MainActivity.java
@@ -0,0 +1,21 @@
+package com.example.lab1_18056521_hahuyhoang;
+
+import androidx.annotation.ContentView;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.util.zip.Inflater;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main3);
+
+    }
+}
